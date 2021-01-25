421cf8365429c5b1ec021ca5738854efb3a7f43d
Paavani-git <pavanipoluru.21.01@gmial.com>
2021-01-25 23:49:35 +0530

    First commit Message

--- disable.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://devnote.in/wp-content/uploads/2020/04/devnote.png")
-
-file = open("appa.png", "wb")
-file.write(response.content)
-print (response.content)
-file.close()
